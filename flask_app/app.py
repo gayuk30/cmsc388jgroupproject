de9aa1eaa9bc7361611a38fe7cd99d3629c85f93
aryananwar <aryananwar221@gmail.com>
2024-05-05 12:49:37 -0400

    added username to reviews

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, redirect, request, url_for
-from flask_login import current_user
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 import certifi
 import os
-from flask_bcrypt import bcrypt
 
 
 app = Flask(__name__)
@@ -25,6 +23,7 @@ def login():
         user = mongo.db.users.find_one({'username': username, 'password': password})
         if user:
             # User found, log them in and redirect to reviews page
+            session['user'] = user['username']
             return redirect(url_for('reviews'))
         else:
             # User not found, display an error message
@@ -38,8 +37,11 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        mongo.db.users.insert_one({'username': username, 'password': password})
-        return redirect(url_for('login'))
+        if mongo.db.users.find_one({'username': username}) is not None:
+            return render_template('register.html', error='Username already exists, try logging in.')
+        else:
+            mongo.db.users.insert_one({'username': username, 'password': password})
+            return redirect(url_for('login'))
     else:
         return render_template('register.html')
 
@@ -53,7 +55,11 @@ def projects():
 
 @app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
-    if user:
-        return render_template('/reviews.html', username=user['username'])
+    user = session.get('user')
+    if user is not None:
+        # User is logged in, pass the username into the template so when they leave a review, it will be associated with their username
+        print(user)
+        return render_template('/reviews.html', username=session['user'])
     else:
-        return redirect(url_for('login'))
+        #If the user is not logged in, they can still view the reviews page, they just cannot leave a review
+        return render_template('/reviews.html')
